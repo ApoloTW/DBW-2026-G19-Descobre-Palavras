da500121e0a5350e1fa07fd040f1261740d750b2
Vigast <jg.vigast@gmail.com>
2026-03-16 22:14:01 +0000

    Criação da estrutura para o projeto. Praticar react em App.jsx.  Em components vai tudo o material compartido entre as paginas. Pages, la cantidad de paginas que tendremos.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025-2026 @UMA - Descobre palavras</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+import "../styles/navbar.css"
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        Descobre Palavras
+      </div>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/about">Sobre Nós</Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
