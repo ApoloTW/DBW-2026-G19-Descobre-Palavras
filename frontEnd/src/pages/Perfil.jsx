71e48c0c4edff482c71c518cfebdcdd87f46c442
Jose Farinha <jm.farinha04@gmail.com>
2026-04-21 20:13:03 +0100

    Fundo de Jogo atualizado

--- frontEnd/src/pages/Perfil.jsx
+++ frontEnd/src/pages/Perfil.jsx
@@ -211,30 +211,6 @@ function RecordePessoal() {
   )
 }
 
-
-function ChangeToGreen() {
-  document.documentElement.style.setProperty(
-    '--bg-gradient',
-    'radial-gradient(circle at center, #16a34a 0%, #14532d 40%, #052e16 100%)',
-  );
-  document.documentElement.style.setProperty('--card-bg', '#5c9d1f');
-  document.documentElement.style.setProperty('--accent', '#5c9d1f');
-  document.documentElement.style.setProperty('--button-bg', '#5c9d1f');
-  document.documentElement.style.setProperty('--button-primary', '#5c9d1f');
-  document.documentElement.style.setProperty('--button-primary-hover', '#14532d');
-}
-
-
-
-function Verde() {
-  return (
-    <button onClick={ChangeToGreen}>
-      Tema Verde
-    </button>
-  )
-}
-
-
 function Perfil({ usuario, setFundoAtual, setAvatar }) {
   return (
     <div className="perfilPage">
@@ -250,7 +226,6 @@ function Perfil({ usuario, setFundoAtual, setAvatar }) {
         <div className="perfilColunaDireita">
           <Estatisticas />
           <FundoJogo setFundoAtual={setFundoAtual} />
-          <Verde />
         </div>
       </div>
     </div>
